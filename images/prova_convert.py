22e5357381bf5a6a989109a729e382916300c72c
Maura Pilia <maura.pilia@gmail.com>
2017-04-26 12:41:30 +0200

    resize images

--- images/prova_convert.py
+++ images/prova_convert.py
@@ -5,4 +5,4 @@ import os
 jpegs = glob('*.jpg')
 
 for jpg in jpegs:
-    os.system('convert '+jpg+' -strip -sampling-factor 4:2:0 '+jpg)
+    os.system('convert '+jpg+' -strip -sampling-factor 4:2:0 -resize 50%  New_'+jpg)
