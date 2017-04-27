2a32cbe5f6da50cf0b230658f1376d93549801b9
Maura Pilia <maura.pilia@gmail.com>
2017-04-27 12:06:21 +0200

    smaller still

--- images/prova_convert.py
+++ images/prova_convert.py
@@ -5,4 +5,4 @@ import os
 jpegs = glob('*.jpg')
 
 for jpg in jpegs:
-    os.system('convert '+jpg+' -strip -sampling-factor 4:2:0 -resize 30% -quantize RGB New_'+jpg+'.png')
+    os.system('convert '+jpg+' -strip -sampling-factor 4:2:0 -resize 30% -quality 8 -quantize RGB New_'+jpg+'.png')
